d17e6509ff0952c38e167b66a0b920e31ccfefbf
mkmksto <119323664+mkmksto@users.noreply.github.com>
2023-01-28 22:43:33 +0800

    rough layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,36 @@
 <script setup lang="ts">
-import Laith from './components/Laith.vue'
+import NavBar from './components/NavBar.vue'
+console.log('hello from app')
 </script>
 
 <template>
-    <Laith />
+    <NavBar />
+    <main>
+        <div class="game-box"></div>
+    </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+main {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    width: 100%;
+}
+
+.game-box {
+    background-color: var(--main-color);
+    margin: 2.5rem 2.5rem 2.5rem 5rem;
+    height: 80%;
+    width: 70%;
+    border-radius: 3rem;
+}
+</style>
+
+<style>
+/* global styles */
+:root {
+    --main-color: hsl(11, 71.9%, 53.9%);
+}
+</style>
---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+console.log('hello from nav')
+</script>
+
+<template>
+    <nav></nav>
+</template>
+
+<style scoped>
+nav {
+    position: fixed;
+    height: 100vh;
+    z-index: 50;
+    background-color: black;
+    width: 5rem;
+}
+</style>
