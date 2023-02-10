726ab4ddd4c00cdbccc1244720a78abffded8937
mkmksto <119323664+mkmksto@users.noreply.github.com>
2023-02-10 21:51:51 +0800

    info modal init

---
+++ frontend/src/components/modals/InfoModal.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="modal">
+        <div class="modal-card">
+            <span>Guess the hidden word in 6 tries.</span>
+            <span
+                >With each guess, the colors of each letter changes accodring to how
+                close to the word you are.</span
+            >
+        </div>
+    </div>
+</template>
+
+<style scoped>
+/* */
+</style>
